9ba66392d4d8d0224f7aaf1ac25527dcf93f2d65
Sebastian Burlacu <sebastian19276@gmail.com>
2022-11-29 21:58:12 -0500

    new config files for ldap that create a new environment with default basic entries

---
+++ config-files/ldap/initialSetup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+DEBIAN_FRONTEND=noninteractive apt install -y slapd ldap-utils libnss-ldap libpam-ldap
+systemctl stop slapd
+mv /var/lib/ldap/data.mdb /var/lib/ldap/data.mdb.old
+slapadd -l initialSetup.ldif
+chown openldap. /var/lib/ldap/data.mdb
+systemctl start slapd
